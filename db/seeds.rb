48d180793745fafdc6b53ba2033319d360322036
basem909 <basemshams30@gmail.com>
2022-10-08 23:27:30 -0300

    Create greetings model

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create!(greetings: 'Hello, good morning!')
+  Greeting.create!(greetings: 'Hallo, guten morgen!')
+
+ Greeting.create!(greetings: 'Hola, Buenos días!')
+  Greeting.create!(greetings: 'أهلا، صباح الخير')
+
+ Greeting.create!(greetings: 'Merhaba')
