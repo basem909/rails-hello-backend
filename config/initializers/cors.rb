a8e1433e39df33157067eb9855d091898209aef5
basem909 <basemshams30@gmail.com>
2022-10-09 00:49:29 -0300

    Add cors configs

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://backend-hello-rails.herokuapp.com/"
+    origins "example.com"
 
     resource "*",
       headers: :any,
