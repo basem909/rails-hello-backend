7d85356e4db012b7e381db876b7083307902f449
basem909 <basemshams30@gmail.com>
2022-10-08 23:40:36 -0300

    Add controller and rout

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::GreetingsController < ApplicationController
+  def index
+        @greetings = Greeting.all.sample.greetings
+        render json: { greet: @greetings }
+      end
+end
